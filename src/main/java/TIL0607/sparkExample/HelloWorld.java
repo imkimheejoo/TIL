7a56c32b5b6dc168ab0f124b6f35620e2f990783
imkimheejoo <jujudamdadi@naver.com>
2019-06-08 20:38:36 +0900

    feat: spark template engine 중 handlebar engine 사용

--- src/main/java/TIL0607/sparkExample/HelloWorld.java
+++ src/main/java/TIL0607/sparkExample/HelloWorld.java
@@ -1,10 +1,16 @@
 package TIL0607.sparkExample;
 
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.*;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        staticFiles.location("/static"); // ->src/resources/static 에서 정적파일을 관리함
+        staticFiles.location("/templates"); // ->src/resources/templates 에서 정적파일을 관리함
         //이름을 동적으로 값이 바뀌도록 전달 -> 콜론
         // /hello/heejoo로 요청하면 웹에 Hello heejoo! 이렇게 뜸
         get("/hello/:name", (req, res) ->
@@ -21,9 +27,46 @@ public class HelloWorld {
                 "Get : Hi " + req.queryParams("name") + " 나이는" + req.queryParams("age") + "!"
         );
 
-        post("/hi", (req, res) ->
-                "Post : Hi " + req.queryParams("name") + " 나이는" + req.queryParams("age") + "!"
+//        post("/hi", (req, res) ->
+//                "Post : Hi " + req.queryParams("name") + " 나이는" + req.queryParams("age") + "!"
+//        );
+
+        // 이렇게 하면 굉장히 번거롭고 귀찮음
+        //이를 보완하기 위해 등장한것이 template engine 이라는 도구
+        /*
+        post("/hi", (req, res) ->{
+            return "<html>" +
+                    "<body>"+
+                "<h1>회원 가입 결과</h1>"+
+                    "이름 : " +req.queryParams("name")+
+                    "<br />"+
+                    "<br />"+
+                    "나이 : " +req.queryParams("age")+
+                    "<br />"+
+                    "<br />"+
+                    "</body>"+
+                    "</html>";
+                }
         );
+        */
+
+        post("/hi", (req, res) -> {
+                    Map<String, Object> model = new HashMap<>();
+                    model.put("name", req.queryParams("name"));
+                    model.put("age", req.queryParams("age"));
+
+                    return render(model, "result.html");
+                }
+        );
+
+    }
+
+    //template engine 추가
+    //이걸 이용해서 값을 html에 전달해 웹을 동적으로 만들 수 있음
+    // 값을 전달하는 방법은 Map을 이용
+    //template Path(어디로 보낼지)가 두번째 인자
+    public static String render(Map<String, Object> model, String templatePath) {
+        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
     }
 
 
--- src/main/java/TIL0607/sparkExample/Note.md
+++ src/main/java/TIL0607/sparkExample/Note.md
@@ -4,3 +4,9 @@
 - static파일을 어떻게 관리할거냐 -> 기본은 src/main/resources
 - static file의 로케이션은 지정 가능
 
+- 정적인 웹( ex. form.html)이아니라 동적인(데이터를 디비에서 가져옴,사용자에 따라 다른 웹페이지를 보여주는것) 웹
+- 동적인 웹을 만들기 위해 쓰는것이 template engine
+
+- template engine 을 써야하는 이유 -> HelloWorld의 post에 필기
+-template engine 중 handlebar engine 이 있음(mustache 같은 것)
+- 동적인 웹은 spark에서 static이 아니라 templates라는 디렉토리에 파일이 위치해야 템플릿 엔진이 작동됨
