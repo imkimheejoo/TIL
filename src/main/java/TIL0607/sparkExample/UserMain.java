7208c98739a2250b28fe823da7f915220617650d
imkimheejoo <jujudamdadi@naver.com>
2019-06-09 23:42:49 +0900

    feat: JavaScript 기초

--- src/main/java/TIL0607/sparkExample/UserMain.java
+++ src/main/java/TIL0607/sparkExample/UserMain.java
@@ -12,17 +12,19 @@ import static spark.Spark.*;
 
 public class UserMain {
     public static void main(String[] args) {
+        externalStaticFileLocation("src/main/resources/templates");
         staticFiles.location("/static");
 
         List<User> users = new ArrayList<>();
 
         post("/users", (req, res) -> {
-                    User user = new User();
-                    user.setName(req.queryParams("name"));
-                    user.setAge(req.queryParams("age"));
-                    users.add(user);
-                    Map<String, Object> model = new HashMap<>();
-                    model.put("users", users);
+                User user = new User();
+                user.setName(req.queryParams("name"));
+                user.setAge(req.queryParams("age"));
+                users.add(user);
+                Map<String, Object> model = new HashMap<>();
+                model.put("users", users);
+                System.out.println("ㅋㅋ");
 
                     return render(model, "result.html");
                 }
