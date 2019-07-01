5d92ada6fbe7abef461fd1144f4f38524b2a5cb2
imkimheejoo <jujudamdadi@naver.com>
2019-07-01 14:13:36 +0900

    feat: Java 8 in action 자바8의 등장이유

---
+++ src/main/java/TIL0701/Compare.java
@@ -0,0 +1,11 @@
+package TIL0701;
+
+public class Compare {
+    /**
+     * 두 송장 ID를 비교하는 메서드
+     * 이 메서드를 sort의 인자로 전달
+     */
+    public int comparaeUsingCustomerId(String inv1,String inv2) {
+
+    }
+}
